f9df716cbf477a0295a47d8ca7ccb241ff635093
Ning Sun <sunning@greptime.com>
2025-08-12 11:41:58 +0800

    chore: add more logs

--- sqlx-postgres/src/connection/stream.rs
+++ sqlx-postgres/src/connection/stream.rs
@@ -66,6 +66,7 @@ impl PgStream {
     where
         T: FrontendMessage,
     {
+        println!("write {}", &message);
         self.write_msg(message)?;
         self.flush().await?;
         Ok(())
@@ -81,6 +82,7 @@ impl PgStream {
         // DO NOT modify `buf` unless a full message has been read
         self.inner
             .try_read(|buf| {
+                println!("recv_unchecked {}", &buf);
                 // all packets in postgres start with a 5-byte header
                 // this header contains the message type and the total length of the message
                 let Some(mut header) = buf.get(..5) else {
